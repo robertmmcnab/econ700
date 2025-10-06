bccdf0cfeb4dfbf125cbf0553eb6c7e48a8492a7
rmcnab67 <rmmcnab@gmail.com>
2025-10-06 15:51:41 -0400

    rev

--- open-links.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll("a[href]").forEach(function(link) {
-    // skip anchor-only links (#) so scroll works normally
-    if (!link.getAttribute("href").startsWith("#")) {
-      link.setAttribute("target", "_blank");
-    }
-  });
-});
